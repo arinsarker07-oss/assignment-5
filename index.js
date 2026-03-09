85b2896f39affac9f41eb5004ae6676a8f7c82e1
arinsarker07-oss <arinsarker07@gmail.com>
2026-03-09 16:42:56 +0600

    modal add

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ const BTNclick = (clickedbutton) => {
     allbutton.forEach(btt => {
         btt.classList.add("btn-soft")
     });
-    clickedbutton.classList.remove("btn-soft") 
+    clickedbutton.classList.remove("btn-soft")
 
-const statusType = clickedbutton.innerText.toLowerCase().trim();
+    const statusType = clickedbutton.innerText.toLowerCase().trim();
     if (statusType === "all") {
         displayLevelwords(allIssues);
-    } 
+    }
     else if (statusType === "open") {
         const openData = allIssues.filter(item => item.status === "open");
         displayLevelwords(openData);
-    } 
+    }
     else if (statusType === "close") {
         const closedData = allIssues.filter(item => item.status === "closed");
         displayLevelwords(closedData);
@@ -44,16 +44,16 @@ const statusType = clickedbutton.innerText.toLowerCase().trim();
 const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
 fetch(url)
     .then((response) => response.json())
-    .then((result) =>{
-         allIssues = result.data
+    .then((result) => {
+        allIssues = result.data
         displayLevelwords(allIssues)
-    } )
+    })
 
 
 const displayLevelwords = (words) => {
     const allcardsection = document.getElementById("all-card-section");
     allcardsection.innerHTML = "";
-document.getElementById("issue-count").innerText = `${words.length} Issues`;
+    document.getElementById("issue-count").innerText = `${words.length} Issues`;
     words.forEach(word => {
         const card = document.createElement("div");
         card.className = "h-full";
@@ -77,7 +77,7 @@ document.getElementById("issue-count").innerText = `${words.length} Issues`;
         const islabel = word.labels.map(label => {
             let eatchLable = ""
             const cleanLabel = label.toLowerCase().trim();
-            
+
             if (cleanLabel === "bug") {
                 eatchLable = ` <div class="badge badge-error badge-soft badge-sm text-[9px] gap-1 py-2 px-2 whitespace-nowrap">
                             <i class="fa-solid fa-bug"></i> BUG
@@ -103,13 +103,13 @@ document.getElementById("issue-count").innerText = `${words.length} Issues`;
                            <i class="fa-solid fa-life-ring"></i>DOCUMENTATION
                         </div>`
             }
-             return eatchLable;
+            return eatchLable;
         }).join("");
 
 
-    card.style.cursor = "pointer";
+
         card.innerHTML = `
-            <div onclick=" document.getElementById('my_modal_1').showModal()" class="card bg-white shadow-sm border-t-4 border-${color}-500 h-full">
+            <div  class="card bg-white shadow-sm border-t-4 border-${color}-500 h-full">
                 <figure class="flex justify-between p-4 pb-2">
                     <div class="h-7 w-7 rounded-full bg-${color}-100 flex items-center justify-center">
                        ${statusicon}
@@ -134,6 +134,8 @@ document.getElementById("issue-count").innerText = `${words.length} Issues`;
                 </div>
             </div> 
         `;
+        card.onclick = () => showDetails(word.id);
+        card.classList.add("cursor-pointer");
         allcardsection.append(card);
     });
 }
@@ -141,12 +143,75 @@ document.getElementById("issue-count").innerText = `${words.length} Issues`;
 document.getElementById("btn-Search").addEventListener("click", () => {
     const input = document.getElementById("input-Search");
     const searchText = input.value.toLowerCase();
-       if (searchText!=="") {
-         const searchUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
+    if (searchText !== "") {
+        const searchUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
         fetch(searchUrl)
             .then(res => res.json())
             .then(result => {
                 displayLevelwords(result.data);
             })
-       }
+    }
 });
+
+const showDetails = (id) => {
+    const detailUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    fetch(detailUrl)
+        .then(res => res.json())
+        .then(result => {
+            const issue = result.data;
+            const modalcard = document.getElementById("modal-card");
+            modalcard.innerHTML = "";
+            const mcard = document.createElement("div");
+
+            const Mstatus = issue.status
+            console.log(Mstatus);
+            
+            let mfstatus = ""
+            if (Mstatus=== "open") {
+                mfstatus = `<div class="badge badge-success badge-soft badge-sm py-2 px-2 uppercase">${issue.status}</div>`
+            }
+            else {
+               mfstatus = `<div class="badge badge-primary badge-soft badge-sm py-2 px-2 uppercase">${issue.status}</div>`
+            }
+
+            const Mispriority = issue.priority
+            let Mbadgecolor = ""
+            if (Mispriority === "high") {
+                Mbadgecolor = `<div class="badge badge-error badge-soft text-[16px] font-bold uppercase">${issue.priority}</div>`
+            }
+            else if (Mispriority === "medium") {
+                Mbadgecolor = `<div class="badge badge-warning badge-soft text-[16px] font-bold uppercase">${issue.priority}</div>`
+            }
+            else {
+                Mbadgecolor = `<div class="badge badge-Info badge-soft text-[16px] font-bold uppercase">${issue.priority}</div>`
+            }
+            mcard.innerHTML = `
+                <div class="max-w-xl mx-auto p-4">
+                    <div class="card bg-base-100 shadow-sm border border-base-200 p-5 rounded-xl font-sans">
+                        <h2 class="text-3xl font-bold text-slate-800 mb-4">${issue.title}</h2>
+                        
+                        <div class="flex items-center gap-3 text-sm text-slate-500 mb-6">
+                            ${mfstatus}
+                            <span>•</span>
+                            <span>Opened by <span class="font-medium text-slate-600">${issue.author}</span></span>
+                            <span>•</span>
+                            <span>${new Date(issue.createdAt).toLocaleDateString()}</span>
+                        </div>
+                        <p class="text-slate-500 text-lg leading-relaxed mb-7">${issue.description}</p>
+                        <div class="bg-slate-50/50 rounded-xl p-4 flex justify-between items-start">
+                            <div class="flex flex-col gap-2">
+                                <span class="text-slate-400 text-lg">Assignee:</span>
+                                <span class="text-slate-800 font-bold text-xl">${issue.author}</span>
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <span class="text-slate-400 text-lg">Priority:</span>
+                               ${Mbadgecolor}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            modalcard.append(mcard);
+            document.getElementById("issue_modal").showModal();
+        });
+};
